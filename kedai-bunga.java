d942393ddbac3a0b06f790b910f5efcdae6c0167
alip <132419867+alip-1kpd1@users.noreply.github.com>
2023-07-30 16:57:33 +0800

    kedai-bunga.java

---
+++ kedai-bunga.java
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Kedai Bunga XYZ</title>
+        <script>
+            function tambahketroli(){
+                //kode untuk menguruskan tindakan tambah ke troli
+                alert('produk telah ditambah ke troli!');
+            }
+            </script>
+    </head>
+    <body>
+        <h1>Selamat datang ke Kedai Bunga XYZ</h1>
+        <h2>Senarai produk</h2>
+        <div class="produk">
+            <img src="Bunga-Mawar-1.png" alt="Bunga Mawar" width="40%">
+            <h3>Bunga Mawar</h3>
+            <p>Harga:RM10.00</p>
+            <button onclick="tambahketroli()">Tambah ke troli</button>
+        </div>
+        <div class="produk">
+            <img src="092330900_1567081124-orchid-4068853_1920.webp" alt="Bunga Anggrek" width="40%">
+            <h3>Bunga Anggrek</h3>
+            <p>Harga:RM15.00</p>
+            <button onclick="tambahketroli()">Tambah ke troli</button>
+    
+        </div>
+        <!--menambah ke produk lain-->
+    </body>
+</html>
